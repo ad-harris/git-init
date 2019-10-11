202d92813eb097c5f2020f857b8482ad5f1cb61f
Alex <ad.harris@outlook.com>
2019-10-11 14:41:38 +0100

    Commit-Message

---
+++ app/routes.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const router = express.Router()
+
+// Add your routes here - above the module.exports line
+
+module.exports = router
+router.post('/juggling-balls-answer', function (req, res) {
+
+// Make a variable and give it the value from 'juggling-balls'
+  var jugglingBalls = req.session.data['how-many-balls']
+
+
+// Check whether the variable matches a condition
+  if (jugglingBalls == "5 or more"){
+    // Send user to next page
+    res.redirect('/juggling-tricks')
+  }
+  else {
+    // Send user to ineligible page
+    res.redirect('/ineligible')
+  }
+
+
+})
